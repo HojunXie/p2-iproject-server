2a28fd5589ec1de5d50b39956b15d96e2ded2057
HojunXie <Hojun.xie2@gmail.com>
2021-09-23 12:22:15 +0700

    deploy 3

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const errorHandler = require('./middlewares/errHandler')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
-app.use(router)
-app.use(errorHandler)
+app.use('/', router)
 
 app.listen(port, () => {
   console.log("App is listening to ", port)
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
-var express = require('express');
+const express = require('express');
 const movieRouter = require('./movieRoutes');
 const threadRouter = require('./threadRoutes');
 const topicRouter = require('./topicRoutes');
 const userRouter = require('./userRoutes');
-var router = express.Router();
+const errorHandler = require('../middlewares/errHandler')
+const router = express.Router();
 
 router.use("/users", userRouter)
 router.use("/topics", topicRouter)
 router.use("/threads", threadRouter)
 router.use("/movies", movieRouter)
+
+router.use(errorHandler)
 
 module.exports = router;
