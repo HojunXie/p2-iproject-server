24ce5389c02ef6ab535ef28e0b49ba52ef4d4fe9
HojunXie <Hojun.xie2@gmail.com>
2021-09-23 15:58:55 +0700

    deploy

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const { User } = require('../models')
 const { compare } = require('../helpers/bcrypt')
-const { generateToken } =  require('../helpers/JWT')
+const { generateToken } =  require('../helpers/jwt')
 
 class UserController {
   static async register (req, res, next) {
